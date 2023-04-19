92a5909933ddff9635a9b3695ce70233a51f50f0
Timbojones <teejay.napoleao@gmail.com>
2023-04-20 00:57:24 +0800

    updated scripts

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/BC68BEB6-5DD5-4862-AAFF-75738A8DE991.jpg") {
+    myImage.setAttribute("src", "images/IMG-4974-positive.jpg");
+  } else {
+    myImage.setAttribute("src", "images/BC68BEB6-5DD5-4862-AAFF-75738A8DE991.jpg");
+  }
+};
