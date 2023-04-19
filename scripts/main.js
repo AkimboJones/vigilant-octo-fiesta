822970df5ba07a667afe188a8ccb5b5242d763e5
Timbojones <teejay.napoleao@gmail.com>
2023-04-20 01:19:39 +0800

    New parameters

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,27 @@ myImage.onclick = () => {
     myImage.setAttribute("src", "images/BC68BEB6-5DD5-4862-AAFF-75738A8DE991.jpeg");
   }
 };
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `I bet you think she is pretty cute, right ${storedName}?`;
+  }
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `I bet you think she is pretty cute, right ${storedName}?`;
+}
+
+myButton.onclick = function() {
+  setUserName();
+};
+
